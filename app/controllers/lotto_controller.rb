8a2abc48710cd4e13655fc94c6f3ff59b04f2185
adsff123 <adsff123@gmail.com>
2016-04-05 11:45:22 +0000

    lotto_1

--- app/controllers/lotto_controller.rb
+++ app/controllers/lotto_controller.rb
@@ -1,23 +1,28 @@
 class LottoController < ApplicationController
     def pick
-        @picking = ["1","2","3","4","5","6","7","8","9","11","12","13","14","15","16","17","18","19","20",
-        "21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40",
-        "41","42","43","44","45",
-        ]
-        @picking_number1 = @picking.sample
-        @picking_number2 = @picking.sample
-        @picking_number3 = @picking.sample
-        @picking_number4 = @picking.sample
-        @picking_number5 = @picking.sample
-        @picking_number6 = @picking.sample
-        @picking_number7 = @picking.sample
+        @picking = (1..45).to_a.sample(6).sort
         
-        @number_image1 = @picking_number1 + ".PNG"
-        @number_image2 = @picking_number2 + ".PNG"
-        @number_image3 = @picking_number3 + ".PNG"
-        @number_image4 = @picking_number4 + ".PNG"
-        @number_image5 = @picking_number5 + ".PNG"
-        @number_image6 = @picking_number6 + ".PNG"
-        @number_image7 = @picking_number7 + ".PNG"
+        @picking_1 = @picking[0]
+        @picking_2 = @picking[1]
+        @picking_3 = @picking[2]
+        @picking_4 = @picking[3]
+        @picking_5 = @picking[4]
+        @picking_6 = @picking[5]
+        
+        @number_image1 = @picking_1 + ".PNG"
+        @number_image2 = @picking_2 + ".PNG"
+        @number_image3 = @picking_3 + ".PNG"
+        @number_image4 = @picking_4 + ".PNG"
+        @number_image5 = @picking_5 + ".PNG"
+        @number_image6 = @picking_6 + ".PNG"
+        
+        @picking.delete(@picking[0])
+        @picking.delete(@picking[1])
+        @picking.delete(@picking[2])
+        @picking.delete(@picking[3])
+        @picking.delete(@picking[4])
+        @picking.delete(@picking[5])
+        @picking_bonus = @picking.sample
+        @number_image7 = @picking_bonus + ".PNG"
     end
 end
